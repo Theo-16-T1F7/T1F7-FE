c9739b27b9653ed3aff97a8f487e13da2699b38c
Seryeong Lee <72387948+Hediar@users.noreply.github.com>
2023-10-22 03:14:52 +0900

    [style & feat] 글 목록 출력, 뒤로가기, 대략적인 style 적용

--- src/pages/ArticleListPage.tsx
+++ src/pages/ArticleListPage.tsx
@@ -1,18 +1,49 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import * as S from '../styles/StArticleList';
-import { BackArrowButton } from '../styles/icons/SvgIcons';
+import { BackArrowButton, SearchButtonIcon } from '../styles/icons/SvgIcons';
 
 const ArticleListPage = () => {
+  const navigate = useNavigate();
+  const sampleData = [
+    { id: '1', title: '첫 번째 글', date: '2023-09-15' },
+    { id: '2', title: '두 번째 글', date: '2023-09-16' },
+    { id: '3', title: '세 번째 글', date: '2023-09-17' },
+    { id: '4', title: '네 번째 글', date: '2023-09-18' },
+    { id: '5', title: '다섯 번째 글', date: '2023-09-19' }
+  ];
+
   return (
     <>
       <S.ArticleNav>
         <div>쁘띠</div>
+        <SearchButtonIcon />
       </S.ArticleNav>
       <S.ArticleHeader>
-        <div>
+        <div
+          onClick={() => {
+            navigate('/');
+          }}
+        >
           <BackArrowButton />
         </div>
+        <h2>게시판 목록</h2>
       </S.ArticleHeader>
+      <S.DivideTitle />
+      <S.ArticleMain>
+        {sampleData.map((article, idx) => {
+          return (
+            <>
+              <div key={article.id}>
+                <S.ArticleElement>
+                  <div>{article.title}</div>
+                  <div> {article.date}</div>
+                </S.ArticleElement>
+                {idx < sampleData.length - 1 && <S.ArticleDivider />}
+              </div>
+            </>
+          );
+        })}
+      </S.ArticleMain>
     </>
   );
 };
