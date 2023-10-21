98d24dcb5d0109971c797da7c764f298e62baa62
soobbb <jsb8437@naver.com>
2023-10-21 23:28:49 +0900

    [feat] App path

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { RecoilRoot } from 'recoil';
-import './App.css';
-
-const queryClient = new QueryClient();
+import { Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <div className="App">HOME</div>
-      </RecoilRoot>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </>
   );
 }
-
 export default App;
