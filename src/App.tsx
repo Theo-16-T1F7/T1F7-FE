2261fcce86d957a3544d27195c90d4ebb7c2b218
soobbb <jsb8437@naver.com>
2023-10-21 23:41:06 +0900

    [feat] App 파일에 path 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
+import ArticleListPage from './pages/ArticleListPage';
+import ContentsPage from './pages/ContentsPage';
+import NoticeListPage from './pages/NoticeListPage';
+import SearchPage from './pages/SearchPage';
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-      </Routes>
-    </>
+    <Router>
+      <>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/article" element={<ArticleListPage />} />
+          <Route path="/contents" element={<ContentsPage />} />
+          <Route path="/notice" element={<NoticeListPage />} />
+          <Route path="/search" element={<SearchPage />} />
+        </Routes>
+      </>
+    </Router>
   );
 }
+
 export default App;
