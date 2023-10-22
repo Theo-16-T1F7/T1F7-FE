bd945159cd5d133b626c1f88f7062ca879e62b37
soobbb <jsb8437@naver.com>
2023-10-23 01:51:16 +0900

    [feat] 공지사항 테스트

--- src/components/Notice/Notice.tsx
+++ src/components/Notice/Notice.tsx
@@ -1,41 +1,31 @@
-import * as S from './Notice.styled';
+import React from 'react';
+// import * as S from './Notice.styled';
+import { getNotice } from '../../api/notice';
+import { useQuery } from '@tanstack/react-query';
 
-const Notice: React.FC<any> = () => {
-  const dummydata = [
-    {
-      id: '1',
-      content:
-        '안녕하세요 쁘디입니다. 10월 22일 00시 ~ 03시까지 내부 서버 점검이 있을 예정으로 부분 기능이 사용 불가할 수 있을을 미리 알려드립니다.',
-      date: '2023/10/22'
-    },
-    {
-      id: '2',
-      content: '안녕하세요2 쁘디입니다.',
-      date: '2023/10/23'
-    },
-    {
-      id: '3',
-      content: '안녕하세요3 쁘디입니다.',
-      date: '2023/10/24'
-    }
-  ];
+const Notice = () => {
+  const { data, error, isLoading } = useQuery<any>({
+    queryKey: ['getNotice'],
+    queryFn: () => getNotice()
+  });
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+  console.log(data);
   return (
-    <>
-      <S.TextCard>
-        <S.NoticeCard>
-          {dummydata
-            .filter((notice) => notice.id === '1')
-            .map((notice, idx) => {
-              return (
-                <div key={notice.id}>
-                  <div>{notice.content}</div>
-                  <div>{notice.date}</div>
-                </div>
-              );
-            })}
-        </S.NoticeCard>
-      </S.TextCard>
-    </>
+    <div>
+      {Array.isArray(data) &&
+        data.map((notice: any, index: number) => (
+          <div key={index}>
+            <div>{notice.title}</div>
+            <div>{notice.content}</div>
+            <div>{notice.createdAt}</div>
+          </div>
+        ))}
+    </div>
   );
 };
 
