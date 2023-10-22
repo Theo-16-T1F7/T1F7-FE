682e145fd6ca232e071524113e4144b95c7f4578
Seryeong Lee <72387948+Hediar@users.noreply.github.com>
2023-10-22 15:07:01 +0900

    [style] theme.ts -> 기본 컬러들 선언

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -4,6 +4,15 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   ${reset}
 
+  @import url('https://cdn.jsdelivr.net/gh/sunn-us/SUIT/fonts/variable/woff2/SUIT-Variable.css');
+
+  :root {
+    --foreground-rgb: #222222;
+    --background-start-rgb: 214, 219, 220;
+    --background-end-rgb: 255, 255, 255;
+    --base-font: 'SUIT', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
 
 `;
 export default GlobalStyle;
