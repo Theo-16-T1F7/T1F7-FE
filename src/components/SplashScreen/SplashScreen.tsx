c35444f77dfcd63399fecc72c2c25222c6ad21ba
soobbb <jsb8437@naver.com>
2023-10-22 14:55:55 +0900

    [feat] 메인페이지에 스플레시스크린 적용

---
+++ src/components/SplashScreen/SplashScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as S from './SplashScreen.styled';
+
+function SplashScreen() {
+  return (
+    <S.StyledBody>
+      <S.SubText>F와 T사이</S.SubText>
+      <S.MainText>쁘띠.</S.MainText>
+    </S.StyledBody>
+  );
+}
+
+export default SplashScreen;
