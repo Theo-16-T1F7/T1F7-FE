9a26b1c409c298dd146d06c9bd480bc21880260e
soobbb <jsb8437@naver.com>
2023-10-23 17:40:29 +0900

    [feat] footer에 post버튼 생성

---
+++ src/components/Footer/Footer.styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const FooterContainer = styled.div`
+  display: flex;
+  justify-content: center; /* 수평 중앙 정렬 */
+  align-items: center; /* 수직 중앙 정렬 */
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  padding: 10px; // 원하는 패딩 값 추가
+`;
