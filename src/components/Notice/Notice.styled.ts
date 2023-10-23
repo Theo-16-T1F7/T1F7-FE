86cff3efab0fb0be56f72a7002395375ef288f8e
soobbb <jsb8437@naver.com>
2023-10-23 22:04:05 +0900

    [feat] 공지사항 css 수정

--- src/components/Notice/Notice.styled.ts
+++ src/components/Notice/Notice.styled.ts
@@ -8,8 +8,28 @@ export const TextCard = styled.div`
 `;
 export const NoticeCard = styled.div`
   width: 80%;
+  height: 20vw;
   background-color: #ea464a;
   margin-top: 10px;
   margin: 0 auto;
-  border-radius: 10px;
+  border-radius: 5px;
+  padding-left: 20px;
+  padding-top: 10px;
+`;
+export const Title = styled.div`
+  font-weight: bold;
+  font-size: 9px;
+  color: ${(props) => props.theme.whiteColor};
+  margin-bottom: 10px;
+`;
+
+export const Content = styled.div`
+  margin-top: 3px;
+  font-size: 6px;
+  color: ${(props) => props.theme.whiteColor};
+`;
+
+export const CreatedAt = styled.div`
+  font-size: 6px;
+  color: ${(props) => props.theme.whiteColor};
 `;
