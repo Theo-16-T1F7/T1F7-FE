b174b1f4593ed556be753cb5c08918b8f0a27a66
soobbb <jsb8437@naver.com>
2023-10-23 17:40:11 +0900

    [feat] serverapi로 변경

--- src/api/notice.ts
+++ src/api/notice.ts
@@ -1,8 +1,8 @@
-import api from './api';
+import severapi from './serverapi';
 
 export const getNotice = async () => {
   try {
-    const response = await api.get('/api/notice');
+    const response = await severapi.get('/api/notice');
     return response.data.data.noticeList;
   } catch (err) {
     throw new Error(`Error: ${err}`);
