40ce8716141c25f0eb02c54cd8312efd054e2735
soobbb <jsb8437@naver.com>
2023-11-07 16:40:15 +0900

    [feat] 컨텐츠 페이지 생성

---
+++ src/pages/ContentPage/ContentPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../../components/Footer/Footer';
+import * as S from './ContentPage';
+
+const ContentPage: React.FC<any> = () => {
+
+  return (
+    <>
+      ContentPage
+      <Footer />
+    </>
+  );
+};
+
+export default ContentPage;
