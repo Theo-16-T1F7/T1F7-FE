212d10310be0349c5e04fc9a166b32659838d1fc
soobbb <jsb8437@naver.com>
2023-11-13 16:30:16 +0900

    [design] 공지사항 페이지 디자인

--- src/pages/NoticePage/NoticePage.styled.ts
+++ src/pages/NoticePage/NoticePage.styled.ts
@@ -1,1 +1,34 @@
 import styled from 'styled-components';
+
+export const Subheading = styled.h2`
+  display: flex;
+  align-items: center;
+  margin-left: 15px;
+  margin-top: 10px;
+  margin-bottom: 20px;
+  margin-right: 100px; /* 여백 값 조정 가능 */
+  font-weight: 900;
+  font-size: 20px;
+`;
+export const SubheadingText = styled.div`
+  margin-left: 10px;
+`;
+export const NoticeList = styled.div`
+  border-top: 2px solid ${(props) => props.theme.dividerColor};
+  border-bottom: 1px solid ${(props) => props.theme.dividerColor};
+`;
+
+export const NoticeListTitle = styled.div`
+  font-size: 14px;
+  font-weight: 700;
+  color: #424242;
+  margin-top: 10px;
+  margin-left: 30px;
+`;
+export const NoticeListCreateAt = styled.div`
+  font-size: 10px;
+  color: #959595;
+  margin-top: 5px;
+  margin-left: 30px;
+  margin-bottom: 10px;
+`;
