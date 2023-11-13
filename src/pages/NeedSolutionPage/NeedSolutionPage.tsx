35581e6c3bc8bb37fb6267fc70cf633fbfd4a478
soobbb <jsb8437@naver.com>
2023-11-13 16:32:41 +0900

    [feat] 해결이 필요해 페이지 생성(useQuery 수정 필요)

---
+++ src/pages/NeedSolutionPage/NeedSolutionPage.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import * as S from './NeedSolutionPage.styled';
+import Header from '../../components/Header/Header';
+import { BackArrowButton } from '../../styles/icons/BackArrowButton';
+import { useQuery, QueryFunction } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import { getNotice } from '../../api/notice';
+import { formatNoticeDate } from '../../shared/dateUtils';
+import { BackButton } from '../../shared/BackButton';
+
+interface Notice {
+  id: number;
+  title: string;
+  createdAt: string;
+}
+
+const NoticePage: React.FC<any> = () => {
+  const { data, error, isLoading } = useQuery<Notice[], AxiosError>({
+    queryKey: ['getNotice'],
+    queryFn: getNotice as QueryFunction<Notice[]>
+  });
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <>
+      <Header />
+      <S.Subheading>
+        <BackButton />
+        <S.SubheadingText> 해결이 필요해👩‍⚖️</S.SubheadingText>
+      </S.Subheading>
+      {data && data.length > 0 ? (
+        <ul>
+          {data.map((notice: any) => (
+            <div key={notice.id}>
+              <S.NoticeList>
+                <S.NoticeListTitle>{notice.title}</S.NoticeListTitle>
+                <S.NoticeListCreateAt>{formatNoticeDate(notice.createdAt)}</S.NoticeListCreateAt>
+              </S.NoticeList>
+            </div>
+          ))}
+        </ul>
+      ) : (
+        <p>No notices found</p>
+      )}
+    </>
+  );
+};
+export default NoticePage;
