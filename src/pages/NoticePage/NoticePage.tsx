625e5cfdcfc3a06059e05ec6486f1ca92d367552
soobbb <jsb8437@naver.com>
2023-11-13 16:30:34 +0900

    [feat] 공지사항 페이지 리스트 불러오기

--- src/pages/NoticePage/NoticePage.tsx
+++ src/pages/NoticePage/NoticePage.tsx
@@ -1,7 +1,52 @@
 import React from 'react';
 import * as S from './NoticePage.styled';
+import Header from '../../components/Header/Header';
+import { useQuery, QueryFunction } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import { getNotice } from '../../api/notice';
+import { formatNoticeDate } from '../../shared/dateUtils';
+import { BackButton } from '../../shared/BackButton';
+
+interface Notice {
+  id: number;
+  title: string;
+  createdAt: string;
+}
 
 const NoticePage: React.FC<any> = () => {
-  return <>공지사항 게시판입니다.</>;
+  const { data, error, isLoading } = useQuery<Notice[], AxiosError>({
+    queryKey: ['getNotice'],
+    queryFn: getNotice as QueryFunction<Notice[]>
+  });
+  if (isLoading) {
+    return <div>로딩중...</div>;
+  }
+  if (error) {
+    return <div>에러: {error.message}</div>;
+  }
+
+  return (
+    <>
+      <Header />
+      <S.Subheading>
+        <BackButton />
+        <S.SubheadingText>공지사항</S.SubheadingText>
+      </S.Subheading>
+      {data && data.length > 0 ? (
+        <ul>
+          {data.map((notice: any) => (
+            <div key={notice.id}>
+              <S.NoticeList>
+                <S.NoticeListTitle>{notice.title}</S.NoticeListTitle>
+                <S.NoticeListCreateAt>{formatNoticeDate(notice.createdAt)}</S.NoticeListCreateAt>
+              </S.NoticeList>
+            </div>
+          ))}
+        </ul>
+      ) : (
+        <p>No notices found</p>
+      )}
+    </>
+  );
 };
 export default NoticePage;
