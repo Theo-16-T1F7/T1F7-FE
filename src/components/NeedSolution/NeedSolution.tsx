faa8d6e00f260512dbf883a5a1833b388c51a487
Seryeong Lee <72387948+Hediar@users.noreply.github.com>
2023-11-13 22:23:14 +0900

    docs: 더미 데이터 제거

--- src/components/NeedSolution/NeedSolution.tsx
+++ src/components/NeedSolution/NeedSolution.tsx
@@ -17,27 +17,7 @@ const NeedSolution: React.FC<any> = () => {
   if (error) {
     return <div>Error: {error.message}</div>;
   }
-  const dummydata = [
-    {
-      id: '1',
-      title: '해결이 필요해요?1',
-      content: '안녕하세요1 쁘디입니다.',
-      date: '2023/10/22 08:24'
-    },
-    {
-      id: '2',
-      title: '해결이 필요해요?2',
 
-      content: '안녕하세요2 쁘디입니다.',
-      date: '2023/10/23 08:24'
-    },
-    {
-      id: '3',
-      title: '해결이 필요해요?3',
-      content: '안녕하세요3 쁘디입니다.',
-      date: '2023/10/24 08:24'
-    }
-  ];
   return (
     <>
       {getData?.map((data, idx) => {
