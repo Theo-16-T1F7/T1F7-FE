a5fb5d3c40d9973b40a5b51978eeb45fd050c7c1
soobbb <jsb8437@naver.com>
2023-11-15 15:17:50 +0900

    [feat] 컨텐츠 페이지개발중 페이지 적용

--- src/pages/ContentPage/ContentPage.tsx
+++ src/pages/ContentPage/ContentPage.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Footer from '../../components/Footer/Footer';
 import * as S from './ContentPage';
-
+import Developing from '../../shared/Developing';
 const ContentPage: React.FC<any> = () => {
-
   return (
     <>
-      ContentPage
+      <Developing />
       <Footer />
     </>
   );
