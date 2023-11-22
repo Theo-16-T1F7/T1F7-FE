669376187328402e392c8c811b10c46f8e2d794e
soobbb <jsb8437@naver.com>
2023-11-22 17:50:06 +0900

    [feat] SaerchPage 뒤로가기 버튼

--- src/shared/BackButton.tsx
+++ src/shared/BackButton.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { BackArrowButton } from '../styles/icons/BackArrowButton';
+import { SaerchBackButton } from '../styles/icons/SearchBackButton';
 
 export const BackButton = () => {
   const navigate = useNavigate();
@@ -9,6 +10,18 @@ export const BackButton = () => {
   return (
     <div onClick={handleBackClick}>
       <BackArrowButton />
+    </div>
+  );
+};
+
+export const SearchBackButton = () => {
+  const navigate = useNavigate();
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+  return (
+    <div onClick={handleBackClick}>
+      <SaerchBackButton />
     </div>
   );
 };
