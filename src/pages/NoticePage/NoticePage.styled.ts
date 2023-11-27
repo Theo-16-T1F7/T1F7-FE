2b672129445455a88f8b786c2dcf5b7206b82fe4
soobbb <jsb8437@naver.com>
2023-11-27 20:02:51 +0900

    [style] 메인 공지사항 생략 기호(ellipsis)로 대체

--- src/pages/NoticePage/NoticePage.styled.ts
+++ src/pages/NoticePage/NoticePage.styled.ts
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
-
+import { NoticeListTitleLine } from '../../shared/ClampLines';
+import { NoticeListContentLine } from '../../shared/ClampLines';
 export const Subheading = styled.h2`
   display: flex;
   align-items: center;
   margin-left: 15px;
   margin-top: 10px;
   margin-bottom: 20px;
-  margin-right: 100px; /* 여백 값 조정 가능 */
+  margin-right: 100px;
   font-weight: 900;
   font-size: 20px;
 `;
@@ -24,6 +25,8 @@ export const NoticeListTitle = styled.div`
   color: #424242;
   margin-top: 10px;
   margin-left: 30px;
+  margin-right: 30px;
+  ${NoticeListTitleLine}
 `;
 
 export const NoticeListContent = styled.div`
@@ -32,6 +35,8 @@ export const NoticeListContent = styled.div`
   color: #424242;
   margin-top: 10px;
   margin-left: 30px;
+  margin-right: 30px;
+  ${NoticeListContentLine}
 `;
 
 export const NoticeListCreateAt = styled.div`
@@ -40,4 +45,22 @@ export const NoticeListCreateAt = styled.div`
   margin-top: 5px;
   margin-left: 30px;
   margin-bottom: 10px;
+`;
+
+export const NoticeListToggleTitle = styled.div`
+  font-size: 14px;
+  font-weight: 700;
+  color: #424242;
+  margin-top: 10px;
+  margin-left: 30px;
+  margin-right: 30px;
+`;
+
+export const NoticeListToggleContent = styled.div`
+  font-size: 10px;
+  font-weight: 700;
+  color: #424242;
+  margin-top: 10px;
+  margin-left: 30px;
+  margin-right: 30px;
 `;
