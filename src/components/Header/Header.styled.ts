853a6d47839cab2867ef9454b9e3253daf73a492
soobbb <jsb8437@naver.com>
2023-11-27 19:21:48 +0900

    [feat]  Header div-> header테그로 수정

--- src/components/Header/Header.styled.ts
+++ src/components/Header/Header.styled.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Header = styled.div<{ headercolor: string }>`
+export const Header = styled.header<{ headercolor: string }>`
   width: 100%;
   height: 11vh;
   display: flex;
