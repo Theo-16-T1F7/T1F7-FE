c93849af5fd84b4d1ec65f6ed40650df8e603ee0
soobbb <jsb8437@naver.com>
2023-11-27 19:03:31 +0900

    [style] 제목, 내용 생략 기호(ellipsis)로 대체

---
+++ src/shared/ClampLines.ts
@@ -0,0 +1,30 @@
+import { css } from 'styled-components';
+
+export const NoticeCardLine = css`
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+export const PostCardLine = css`
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+export const NoticeListTitleLine = css`
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+export const NoticeListContentLine = css`
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
