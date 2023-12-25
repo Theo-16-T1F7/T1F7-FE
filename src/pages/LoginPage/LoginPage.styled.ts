0086041419be278179d1f40601aec23d991a3255
soobbb <jsb8437@naver.com>
2023-12-25 14:30:06 +0900

    [design] 로그인 페이지 생성

---
+++ src/pages/LoginPage/LoginPage.styled.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const StyledBody = styled.div`
+  height: 100vh;
+  background: #ffffff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+`;
+
+export const MainText = styled.h1`
+  color: black;
+  font-size: #484848;
+  margin-left: 20px;
+  margin-top: 20px;
+`;
+
+export const SubText = styled.h3`
+  color: #ffffff;
+  font-size: 20px;
+  margin: 0;
+`;
+export const LogoPosition = styled.div`
+  padding-bottom: 300px;
+`;
+export const LoginPosition = styled.div`
+
+`;
