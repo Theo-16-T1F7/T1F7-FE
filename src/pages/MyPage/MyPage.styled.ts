f592ca1aaacc4aca3624ac695873defe2098a343
soobbb <jsb8437@naver.com>
2024-01-04 17:10:57 +0900

    [design] 마이페이지 디자인 생성

---
+++ src/pages/MyPage/MyPage.styled.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+export const Subheading = styled.h2`
+  display: flex;
+  align-items: center;
+  margin-left: 15px;
+  margin-top: 10px;
+  margin-bottom: 20px;
+  margin-right: 100px;
+  font-weight: 600;
+  font-size: 20px;
+`;
+export const SubheadingText = styled.div`
+  margin-left: 10px;
+`;
+export const FirstContainer = styled.div`
+  display: flex;
+  padding-left: 6vw;
+  padding-top: 8vw;
+`;
+
+export const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 6vw;
+`;
+
+export const LoginButtonWrapper = styled.div`
+  padding-top: 6vw;
+  &:hover {
+    cursor: pointer;
+  }
+`;
