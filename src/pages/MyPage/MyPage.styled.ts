6a10f70f51e8e9057fbc198397f08bc7ab6e2fcd
soobbb <jsb8437@naver.com>
2024-01-11 22:38:08 +0900

    [style] 내가 쓴 글, 내가 쓴 댓글 레이아웃

--- src/pages/MyPage/MyPage.styled.ts
+++ src/pages/MyPage/MyPage.styled.ts
@@ -16,11 +16,11 @@ export const FirstContainer = styled.div`
   display: flex;
   padding-left: 6vw;
   padding-top: 8vw;
+  background-color: #f9f9f9;
 `;
 export const SecondContainer = styled.div`
   display: flex;
-  padding-left: 6vw;
-  padding-top: 6vw;
+  background-color: #f9f9f9;
 `;
 export const ThirdContainer = styled.div`
   display: flex;
@@ -38,5 +38,45 @@ export const LoginButtonWrapper = styled.div`
   padding-top: 2.5vw;
   &:hover {
     cursor: pointer;
+  }
+`;
+
+export const PostWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 3rem;
+  color: gray;
+  border-top: 6px solid #efefef;
+  border-right: none;
+  border-left: none;
+  border-bottom: 3px solid #efefef;
+  margin-top: 20px;
+`;
+
+export const PostContainer = styled.div<{ isloggedin?: boolean; clicked?: boolean }>`
+  display: flex;
+  width: 50%;
+  justify-content: center;
+  align-items: center;
+  color: ${({ isloggedin }) => (isloggedin ? '#000000' : '#00000066')};
+  cursor: pointer;
+  transition: color 0.3s, border 0.3s;
+  border-bottom: ${({ clicked }) => (clicked ? '2px solid #ea464a' : 'none')};
+  &:hover {
+    color: #ea464a;
+  }
+`;
+
+export const AnswerContainer = styled.div<{ isloggedin?: boolean; clicked?: boolean }>`
+  display: flex;
+  width: 50%;
+  justify-content: center;
+  align-items: center;
+  color: ${({ isloggedin }) => (isloggedin ? '#000000' : '#00000066')};
+  cursor: pointer;
+  transition: color 0.3s, border 0.3s;
+  border-bottom: ${({ clicked }) => (clicked ? '2px solid #ea464a' : 'none')};
+  &:hover {
+    color: #ea464a;
   }
 `;
