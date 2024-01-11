cfa4d0cb8104d669f92b5b706c9a36c54c96fd88
soobbb <jsb8437@naver.com>
2024-01-11 22:38:29 +0900

    [faet] 내가 쓴 글, 내가 쓴 댓글

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -1,6 +1,7 @@
 import * as S from './MyPage.styled';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import { LoginButton, LogoutButton, MyImage, LoginText, ProfileEditButton } from '../../styles/icons/SvgIcons';
 import { BackButton } from '../../shared/BackButton';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
@@ -12,12 +13,23 @@ const MyPage = () => {
   const accessToken = useRecoilValue(accessTokenState);
   const user = useRecoilValue(userState);
   const setUser = useSetRecoilState(userState);
+  const [isPostClicked, setIsPostClicked] = useState(false);
+  const [isAnswerClicked, setIsAnswerClicked] = useState(false);
+
+  const handlePostClick = () => {
+    setIsPostClicked(true);
+    setIsAnswerClicked(false);
+  };
+
+  const handleAnswerClick = () => {
+    setIsAnswerClicked(true);
+    setIsPostClicked(false);
+  };
 
   const handleLogout = async () => {
     try {
       // 로그아웃 요청 보내기
       const url = `${process.env.REACT_APP_SERVER_BASE_URL}/logout/kakao`;
-      console.log(accessToken);
       await axios.post(
         url,
         {},
@@ -74,6 +86,25 @@ const MyPage = () => {
         </S.LoginContainer>
       </S.FirstContainer>
       <Footer />
+      <S.SecondContainer>
+        <S.PostWrapper>
+          {user ? (
+            <>
+              <S.PostContainer isloggedin={user} onClick={handlePostClick} clicked={isPostClicked}>
+                내가 쓴 글
+              </S.PostContainer>
+              <S.AnswerContainer isloggedin={user} onClick={handleAnswerClick} clicked={isAnswerClicked}>
+                내가 쓴 댓글
+              </S.AnswerContainer>
+            </>
+          ) : (
+            <>
+              <S.PostContainer>내가 쓴 글</S.PostContainer>
+              <S.AnswerContainer>내가 쓴 댓글</S.AnswerContainer>
+            </>
+          )}
+        </S.PostWrapper>
+      </S.SecondContainer>
     </>
   );
 };
