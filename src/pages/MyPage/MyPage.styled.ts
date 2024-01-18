f9ac830a3b2d31c718159181a737db817ece424c
soobbb <jsb8437@naver.com>
2024-01-18 16:54:20 +0900

    [design] 마이페이지 디자인 수정

--- src/pages/MyPage/MyPage.styled.ts
+++ src/pages/MyPage/MyPage.styled.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+
+// 프로필 사진, 로그인, 내 정보 수정, 로그아웃 버튼 레이아웃
 export const Subheading = styled.h2`
   display: flex;
   align-items: center;
@@ -18,15 +20,6 @@ export const FirstContainer = styled.div`
   padding-top: 8vw;
   background-color: #f9f9f9;
 `;
-export const SecondContainer = styled.div`
-  display: flex;
-  background-color: #f9f9f9;
-`;
-export const ThirdContainer = styled.div`
-  display: flex;
-  padding-left: 6vw;
-  padding-top: 8vw;
-`;
 
 export const LoginContainer = styled.div`
   display: flex;
@@ -41,6 +34,12 @@ export const LoginButtonWrapper = styled.div`
   }
 `;
 
+// 내가 쓴 글, 댓글 버튼 레이아웃
+export const SecondContainer = styled.div`
+  display: flex;
+  background-color: #f9f9f9;
+`;
+
 export const PostWrapper = styled.div`
   display: flex;
   width: 100%;
@@ -49,7 +48,7 @@ export const PostWrapper = styled.div`
   border-top: 6px solid #efefef;
   border-right: none;
   border-left: none;
-  border-bottom: 3px solid #efefef;
+  border-bottom: 2px solid #efefef;
   margin-top: 20px;
 `;
 
@@ -79,4 +78,37 @@ export const AnswerContainer = styled.div<{ isloggedin?: boolean; clicked?: bool
   &:hover {
     color: #ea464a;
   }
+`;
+
+// 내가 쓴 글, 댓글 리스트 레이아웃
+export const ThirdContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 5vw;
+  margin-top: 3vw;
+
+`;
+
+export const MyListContainer = styled.div`
+  padding-bottom: 2vw;
+  border-bottom: 1px solid #efefef;
+`;
+
+export const MyListTitle = styled.div`
+  font-size: 14px;
+  font-weight: 700;
+  color: #424242;
+  margin-top: 10px;
+`;
+
+export const MyListName = styled.div`
+  font-size: 10px;
+  color: #424242;
+  margin-top: 10px;
+`;
+
+export const MyListCreateAt = styled.div`
+  font-size: 10px;
+  color: #959595;
+  margin-top: 5px;
 `;
