5dacb1ad0201cf5982d66f2dac5d88cbb2e551f4
soobbb <jsb8437@naver.com>
2024-01-24 19:36:29 +0900

    [feat] userId, userNickname API 호출 로직

--- src/pages/Redirection/Redirection.tsx
+++ src/pages/Redirection/Redirection.tsx
@@ -2,9 +2,10 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { getUserId } from '../../api/profile';
-import { useQuery } from '@tanstack/react-query';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { accessTokenState, userState } from '../../atoms/atoms';
+import { getUserInfo } from '../../api/profile';
+import { useQueries, useQuery } from '@tanstack/react-query';
+import { useRecoilState, useSetRecoilState, useRecoilValue } from 'recoil';
+import { accessTokenState, userState, userNicknameState, userIdState } from '../../atoms/atoms';
 import SplashScreen from '../../components/SplashScreen/SplashScreen';
 
 const Redirection = () => {
@@ -12,11 +13,20 @@ const Redirection = () => {
   const [code, setCode] = useState<string | null>(null);
   const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
   const [user, setUser] = useRecoilState(userState);
-  const [userId, setUserId] = useState(null);
+  // const [userId, setUserId] = useState(null);
+  const setUserNickname = useSetRecoilState(userNicknameState);
+  const setUserId = useSetRecoilState(userIdState);
 
-  const { data, error, isLoading } = useQuery<any>({
+  // userId 불러오는 API
+  const { data: userIdData } = useQuery<any>({
     queryKey: ['getUserId'],
     queryFn: () => getUserId()
+  });
+
+  // userNickname 불러오는 API
+  const { data: userInfoData } = useQuery<any>({
+    queryKey: ['getUserInfo'],
+    queryFn: () => getUserInfo()
   });
 
   // URL 인가코드 저장
@@ -28,6 +38,7 @@ const Redirection = () => {
     }
   }, []);
 
+  // 로그인
   useEffect(() => {
     if (code) {
       const url = `${process.env.REACT_APP_SERVER_BASE_URL}/oauth2/kakao/callback?code=${code}`;
@@ -39,20 +50,27 @@ const Redirection = () => {
           sessionStorage.setItem('accessToken', token);
           setAccessToken(token);
           setUser(true);
-          navigate('/');
         })
         .catch((error) => {
           console.error('오류 발생:', error);
         });
     }
-  }, [code, navigate, setAccessToken, setUser]);
+  }, [code, setAccessToken, setUser]);
 
+  // userId
   useEffect(() => {
-    if (data) {
-      setUserId(data);
+    if (userIdData) {
+      setUserId(userIdData);
     }
-  }, [data]);
-  console.log(userId);
+  }, [userIdData]);
+
+  // userNickname
+  useEffect(() => {
+    if (userInfoData) {
+      setUserNickname(userInfoData);
+      navigate('/mypage');
+    }
+  }, [userInfoData, setUserNickname, navigate]);
 
   return (
     <>
