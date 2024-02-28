3db98372a1fd86e0f7adb9075861ceae1e7c0da3
soobbb <jsb8437@naver.com>
2024-02-28 17:41:05 +0900

    [feat] mypage 내가 쓴 글 API 수정

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -6,7 +6,7 @@ import { useState } from 'react';
 import { LoginButton, LogoutButton, LoginText, ProfileEditButton, ProfileImage } from '../../styles/icons/SvgIcons';
 import { BackButton } from '../../shared/BackButton';
 import Footer from '../../components/Footer/Footer';
-import { getMyPost } from '../../api/profile';
+import { getMyPost, getMyAnswer } from '../../api/profile';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { userState, userNicknameState } from '../../atoms/atoms';
 
@@ -15,14 +15,19 @@ interface PostItem {
   title: string;
   nickname: string;
   createdAt: string;
+  content: string;
 }
 
 const MyPage = () => {
-  const { data } = useQuery<PostItem[]>({
+  const { data: myPostData } = useQuery<PostItem[]>({
     queryKey: ['getMyPost'],
     queryFn: getMyPost as QueryFunction<PostItem[]>
   });
-  const myPostData = data;
+  const { data: myAnswerData } = useQuery<PostItem[]>({
+    queryKey: ['getMyAnswer'],
+    queryFn: getMyAnswer as QueryFunction<PostItem[]>
+  });
+  // const myPostData = data;
   const navigate = useNavigate();
   const accessToken = sessionStorage.getItem('accessToken');
   const user = useRecoilValue(userState);
@@ -144,7 +149,21 @@ const MyPage = () => {
       </S.SecondContainer>
       <S.ThirdContainer>
         {user && isAnswerClicked ? (
-          <div>내가 쓴 댓글</div>
+          <>
+            {user && myAnswerData && myAnswerData.length > 0 ? (
+              <>
+                {myAnswerData.map((myAnswer: PostItem) => (
+                  <S.MyListContainer key={myAnswer?.id}>
+                    <S.MyListTitle>{myAnswer?.content}</S.MyListTitle>
+                    <S.MyListName>{myAnswer?.nickname}</S.MyListName>
+                    <S.MyListCreateAt>{myAnswer?.createdAt}</S.MyListCreateAt>
+                  </S.MyListContainer>
+                ))}
+              </>
+            ) : (
+              <></>
+            )}
+          </>
         ) : (
           <>
             {user && myPostData && myPostData.length > 0 ? (
