43c7a387414ab80be538e925b753e75ad328a63c
soobbb <jsb8437@naver.com>
2024-04-10 17:14:38 +0900

    [design] 마이페이지 mbti 추가

--- src/pages/MyPage/MyPage.styled.ts
+++ src/pages/MyPage/MyPage.styled.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 // 프로필 사진, 로그인, 내 정보 수정, 로그아웃 버튼 레이아웃
 export const Subheading = styled.h2`
@@ -37,9 +37,24 @@ export const UserNickname = styled.div`
   size: 7vw;
   font-weight: 700;
   color: #424242;
-  padding-bottom: 4vw;
+  padding-bottom: 1vw;
 `;
 
+export const UserMbti = styled.div<{ mbti: any }>`
+  size: 1vw;
+  font-weight: 600;
+  padding-bottom: 2vw;
+  ${({ mbti }) =>
+    mbti === 'T'
+      ? css`
+          color: blue;
+        `
+      : mbti === 'F'
+      ? css`
+          color: red;
+        `
+      : ''};
+`;
 // 내가 쓴 글, 댓글 버튼 레이아웃
 export const SecondContainer = styled.div`
   display: flex;
@@ -124,4 +139,28 @@ export const MyListCreateAt = styled.div`
   font-size: 10px;
   color: #959595;
   margin-top: 5px;
+`;
+
+export const ProfileImageWrapper = styled.div`
+  display: flex;
+  position: relative;
+  width: 375px; /* 이미지의 원래 크기 */
+  height: 100px; /* 이미지의 원래 크기 */
+  overflow: hidden; /* 이미지가 넘치는 경우를 대비하여 오버플로우를 숨김 */
+`;
+
+export const ProfileImageOverlay = styled.div`
+  position: absolute;
+  top: 15%; /* 이미지의 상단부터 15% 지점에 배치 */
+  left: 0;
+  right: 0;
+  bottom: 0;
+  // background-color: rgba(255, 255, 255, 0.5); /* 투명한 배경색 */
+  z-index: 1; /* 이미지 위로 올리기 */
+`;
+
+export const ProfileImage = styled.img`
+  width: 100%; /* 부모 요소인 ProfileImageWrapper에 꽉 차게 설정 */
+  height: 100%; /* 부모 요소인 ProfileImageWrapper에 꽉 차게 설정 */
+  object-fit: cover; /* 이미지를 컨테이너에 가득 채우고, 가로세로 비율을 유지하면서 잘라냄 */
 `;
