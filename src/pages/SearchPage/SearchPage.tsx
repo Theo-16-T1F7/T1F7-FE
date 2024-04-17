31efc984b2d8bc3cf9712d59f0d68854aaa16956
soobbb <jsb8437@naver.com>
2024-04-17 10:57:20 +0900

    [feat] 검색페이지 개발중 화면으로 변경

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,13 +1,28 @@
+// import React from 'react';
+// import { Subheading } from './SearchPage.styled';
+// import { SearchBar } from './SearchPage.styled';
+// import { SearchBackButton } from '../../shared/BackButton';
+// const SearchPage = () => {
+//   return (
+//     <Subheading>
+//       <SearchBackButton />
+//       <SearchBar placeholder="검색어를 입력하세요" />
+//     </Subheading>
+//   );
+// };
+
+// export default SearchPage;
+
 import React from 'react';
-import { Subheading } from './SearchPage.styled';
-import { SearchBar } from './SearchPage.styled';
-import { SearchBackButton } from '../../shared/BackButton';
-const SearchPage = () => {
+import Footer from '../../components/Footer/Footer';
+import * as S from './SearchPage';
+import Developing from '../../shared/Developing';
+const SearchPage: React.FC<any> = () => {
   return (
-    <Subheading>
-      <SearchBackButton />
-      <SearchBar placeholder="검색어를 입력하세요" />
-    </Subheading>
+    <>
+      <Developing />
+      <Footer />
+    </>
   );
 };
 
