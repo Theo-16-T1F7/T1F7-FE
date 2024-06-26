4aee517c190704764d9bd3d332b2c0bd145f74b2
soobbb <jsb8437@naver.com>
2024-06-26 20:56:49 +0900

    [fix] 리다이렉트 uri 서버로 변경

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -3,8 +3,8 @@ import { LoginScreenIcon, KakaoLogin } from '../../styles/icons/SvgIcons';
 
 const LoginPage = () => {
   const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
-  // const REDIRECT_URI = 'https://www.bbeudde.site/oauth2/kakao/callback';
-  const REDIRECT_URI = 'http://localhost:3000/kakao/oauth';
+  const REDIRECT_URI = 'https://www.bbeudde.site/oauth2/kakao/callback';
+  // const REDIRECT_URI = 'http://localhost:3000/kakao/oauth';
   const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   const loginHandler = () => {
